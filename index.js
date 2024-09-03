3fbcb430fc4c2eb496e008dc95dbe98b155e1e79
Aliyah2003 <aliyahibrahim3@gmail.com>
2024-09-03 16:45:26 +0100

    add the env file and the start script

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
-const port=3000
+const port=process.env.port
 const characters=require('./harrypotter.json')
 app.use(express.json())
 
